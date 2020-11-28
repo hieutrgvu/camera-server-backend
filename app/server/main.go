887a7be2edf713ca5d4cc47c7b2562e5d13dfe42
Hieu Vu <hieutrgvu@gmail.com>
2020-11-29 00:55:55 +0700

    Using database for events
    Signed-off-by: Hieu Vu <hieutrgvu@gmail.com>

--- app/server/main.go
+++ app/server/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"camera-server-backend/controller"
 	"camera-server-backend/handler"
+	"camera-server-backend/pkgs/store"
 )
 
 func main() {
-	c := controller.NewController()
-	s := handler.NewServer(c)
-	s.Run()
+	s, err := store.NewStore()
+	if err != nil {
+		panic(err)
+	}
+
+	c := controller.NewController(s)
+	h := handler.NewHandler(c)
+	h.Run()
 }
