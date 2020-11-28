67ebd71b05f038b56a69bec3c10963136a5fc9a8
datvotien <votidat@gmail.com>
2020-11-28 22:07:49 +0700

    Add feature Get Images
    Signed-off-by: datvotien <votidat@gmail.com>

--- model/events.go
+++ model/events.go
@@ -7,3 +7,9 @@ type Event struct {
 	CameraID   string   `json:"camera_id" binding:"required"`
 	VectorIds  []string `json:"vector_ids" binding:"required"`
 }
+
+// ImageInfo ...
+type ImageInfo struct {
+	ImageId     string `json:"vector_ids"`
+	ImageBase64 string `json:"image_base64"`
+}
